a8600f44034653e89b19a99a8229c390bc07dd4a
Niklas Wahlén <niklas.wahlen@slagkryssaren.com>
2016-10-25 20:57:36 +0200

    Add Google Play support and tests

--- index.js
+++ index.js
@@ -4,137 +4,225 @@
  * MIT Licensed
  */
 
-var request = require('request');
-var Watcher = require('rss-watcher');
+const request = require('request');
+const Watcher = require('rss-watcher');
+const cheerio = require('cheerio');
 
-var WATCHER_EVENTS = {
+const WATCHER_EVENTS = {
   "NEW_ARTICLE": "new article",
   "ERROR": "error"
 };
 
-var REVIEWS_STORES = {
+const REVIEWS_STORES = {
   "APP_STORE": "app-store",
   "GOOGLE_PLAY": "google-play",
 }
 
-var REVIEWS_LIMIT = 50;
+const REVIEWS_LIMIT = 50;
+const DEFAULT_INTERVAL_SECONDS = 300;
 
 var published_reviews = [];
 
 (function() {
   exports.start = function start(config) {
-    var appInformation = {};
-
-    var watcher;
-    watcher = new Watcher(config.feed);
-    watcher.set({
-      feed: config.feed,
-		  interval: config.interval != null ? config.interval : 300
-    });
-
     if (!config.store) {
       // Determine from which store reviews are downloaded
-      config.store = (config.feed.indexOf("itunes.apple") > -1) ? REVIEWS_STORES.APP_STORE : REVIEWS_STORES.GOOGLE_PLAY;
+      config.store = (config.appId.indexOf("\.") > -1) ? REVIEWS_STORES.GOOGLE_PLAY : REVIEWS_STORES.APP_STORE;
     }
 
-    watcher.on(WATCHER_EVENTS.NEW_ARTICLE, function onNewArticle(review) {
-  		if (!review) {
-  			if (config.debug) console.log("WARNING: Received null or undefined review");
-  		} else if (isAppInformationEntry(review)) {
-        if (config.debug) console.log("INFO: Received new app information");
-        updateAppInformation(config, review, appInformation);
-      } else if (!reviewPublished(config, review)) {
-        if (config.debug) console.log("INFO: Received new review: " + review);
-  	    message = slackMessage(review, config, appInformation);
-        postToSlack(message, config);
-        markReviewAsPublished(config, review);
-      } else if (reviewPublished(config, review)) {
-        if (config.store === REVIEWS_STORES.APP_STORE) {
-          if (config.debug) console.log("INFO: Review already published: " + review.description);
-        }
-      }
-    });
+    var appInformation = {};
 
-    watcher.on(WATCHER_EVENTS.ERROR, function onError(error) {
-      return console.error("ERROR: for new review: " + error);
-    });
-
-    return watcher.run(function run(error, entries) {
-      if (error != null) return console.error("ERROR: Could not parse feed " + config.feed + ", " + error);
-
-      if (entries == null) return console.log("WARNING: Currently no reviews available for " + config.feed);
-
-      // Parse existing entries for app information
-      for (var i = 0; i < entries.length; i++) {
-        var entry = entries[i];
-
-		    // Mark any eixsting reviews as published
-        markReviewAsPublished(config, entry)
-        
-        updateAppInformation(config, entry, appInformation);
+    if (config.store === REVIEWS_STORES.APP_STORE) {
+      if (!config.region) {
+        config.region = "us";
       }
 
-      if (config.debug) {
-        var appString = null;
-        console.log("INFO: Started watching app: " + (config.appName ? config.appName : appInformation.appName));
-        var store = storeName(config);
-        var welcomeMessage = {
-          "username": config.botUsername,
-          "icon_url": config.botIcon,
-          "channel": config.channel,
-          "attachments": [
-            {
-              "mrkdwn_in": ["pretext", "author_name"],
-              "fallback": "This channel will now receive " + store + " reviews for " + (config.appName ? config.appName : appInformation.appName),
-              "pretext": "This channel will now receive " + store + " reviews for ",
-              "author_name": config.appName ? config.appName : appInformation.appName,
-              "author_icon": config.appIcon ? config.appIcon : appInformation.appIcon
-            }
-          ]
+      if (!config.feed) {
+        config.feed = "https://itunes.apple.com/" + config.region + "/rss/customerreviews/id=" + config.appId + "/sortBy=mostRecent/xml";
+      } else {
+        console.log("INFO: Setting feed directly is deprecated, see updated docs at https://github.com/wahni/reviews-to-slack");
+      }
+
+      var watcher;
+      watcher = new Watcher(config.feed);
+      watcher.set({
+        feed: config.feed,
+        interval: config.interval != null ? config.interval : DEFAULT_INTERVAL_SECONDS
+      });
+
+      watcher.on(WATCHER_EVENTS.NEW_ARTICLE, function onNewArticle(item) {
+        if (!item) {
+          if (config.debug) console.log("WARNING: Received null or undefined review");
+          return;
         }
-        postToSlack(welcomeMessage, config);
-      } 
-    });
+
+        if (isAppInformationEntry(item)) {
+          if (config.debug) console.log("INFO: Received new app information");
+          updateAppInformation(config, item, appInformation);
+          return;
+        }
+
+        review = exports.parseAppStoreReview(item, config, appInformation);
+
+        if (!exports.reviewPublished(review)) {
+          if (config.debug) console.log("INFO: Received new review: " + review);
+          message = exports.slackMessage(review, config, appInformation);
+          exports.postToSlack(message, config);
+          exports.markReviewAsPublished(config, review);
+        } else if (exports.reviewPublished(config, review)) {
+          if (config.debug) console.log("INFO: Review already published: " + review.text);
+        }
+      });
+
+      watcher.on(WATCHER_EVENTS.ERROR, function onError(error) {
+        return console.error("ERROR: for new review: " + error);
+      });
+
+      return watcher.run(function run(error, entries) {
+        if (error != null) return console.error("ERROR: Could not parse feed for " + config.appId + ", " + error);
+
+        if (entries == null) return console.log("WARNING: Currently no reviews available for " + config.appId);
+
+        // Parse existing entries for app information
+        for (var i = 0; i < entries.length; i++) {
+          var item = entries[i];
+
+          var review = exports.parseAppStoreReview(item, config, appInformation);
+
+          // Mark any eixsting reviews as published
+          exports.markReviewAsPublished(config, review)
+          
+          updateAppInformation(config, item, appInformation);
+        }
+
+        if (config.debug) {
+          console.log("INFO: Started watching app: " + (config.appName ? config.appName : appInformation.appName));          
+          var welcome = welcomeMessage(config, appInformation);
+          exports.postToSlack(welcome, config);
+        }
+      });
+    } else {
+      exports.fetchGooglePlayReviews(config, appInformation, function (initialReviews) {
+        for (var i = 0; i < initialReviews.length; i++) {
+          var initialReview = initialReviews[i];
+          exports.markReviewAsPublished(config, initialReview);
+        }
+
+        var interval_seconds = config.interval ? config.interval : DEFAULT_INTERVAL_SECONDS;
+
+        setInterval(function(config, appInformation) {
+          if (config.debug) console.log("INFO: [" + config.appId + "] Fetching Google Play reviews");
+
+          exports.fetchGooglePlayReviews(config, appInformation, function (reviews) {
+            exports.handleFetchedGooglePlayReviews(config, appInformation, reviews);
+          });
+        }, interval_seconds * 1000, config, appInformation);
+
+        if (config.debug) {
+          console.log("INFO: [" + config.appId + "] Started watching app: " + (config.appName ? config.appName : appInformation.appName));          
+          var welcome = welcomeMessage(config, appInformation);
+          exports.postToSlack(welcome, config);
+        }
+      });
+    }
   }
 }).call(this);
 
-var storeName = function(config) {
-  if (config.store === REVIEWS_STORES.APP_STORE)  {
-    return "App Store";
-  } else if (config.store === REVIEWS_STORES.GOOGLE_PLAY) {
-    return "Google Play";
-  } else {
-    return null;
+exports.handleFetchedGooglePlayReviews = function(config, appInformation, reviews) {
+  for (var n = 0; n < reviews.length; n++) {
+    var review = reviews[n];
+    if (exports.reviewPublished(review)) {
+      continue;
+    }
+    if (config.debug) console.log("INFO: [" + config.appId + "] Found a new Google Play review: " + review.text);
+    message = exports.slackMessage(review, config, appInformation);
+    exports.postToSlack(message, config);
+    exports.markReviewAsPublished(config, review);
   }
 }
 
-var markReviewAsPublished = function(config, review) {
-  if (!review || reviewPublished(config, review)) return;
+exports.fetchGooglePlayReviews = function(config, appInformation, callback) {
+  form = {'xhr': 1, 'id': config.appId, 'reviewSortOrder': 0, 'pageNum': 0, 'reviewType': 0};
+  if (config.region) {
+    form['gl'] = config.region;
+    form['hl'] = config.region;
+  }
+  result = request.post({
+    url: 'https://play.google.com/store/getreviews',
+    form: form
+  },
+    function (error, response, body) {
+      if (config.debug) console.log("INFO: [" + config.appId + "] Got response for request " + JSON.stringify(response.request));
+      if (error) {
+        console.error("ERROR: [" + config.appId + "] Could not fetch Google Play reviews, " + error);
+      } else {
+        // The body contains some unwanted prefix data, then a JSON matrix (starts with '[')
+        // The actual HTML body that contains the reviews is at position (0,2).
+        body_string = String(body);
+        json_body = JSON.parse(body_string.substring(body_string.indexOf('[')));
+        reviews_body = json_body[0][2];
 
-  var review_id = reviewId(config, review);
-  if (!review_id) return;
+        if (!reviews_body) {
+          if (config.debug) console.log("INFO: [" + config.appId + "] No reviews in body: " + body_string);
+          callback([]);
+        }
+
+        $ = cheerio.load(reviews_body);
+
+        var html_reviews = $('.single-review');
+
+        var reviews = html_reviews.map(function (i, element) {
+          // this === element
+          var review = {};
+          review.id = $(this).find('.review-header').attr('data-reviewid');
+          review.date = $(this).find('.review-header .review-date').text().trim();
+          review.title = $(this).find('.review-body .review-title').text().trim();
+          review.text = $(this).find('.review-body').first().contents().filter(function() {
+            return this.type === 'text';
+          }).text().trim();
+          review.author = $(this).find('.review-header .author-name').text().trim();
+          ratingData = $(this).find('.current-rating').attr('style');
+          review.rating = parseInt(ratingData.replace(/[^\d]/g, '')) / 20;
+          review.link = 'https://play.google.com' + $(this).find('.reviews-permalink').attr('href'),
+
+          review.storeName = "Google Play";
+
+          return review;
+        });
+
+        console.log("INFO: [" + config.appId + "] Found " + reviews.length + " reviews");
+
+        callback(reviews);
+      }
+    }
+  );
+}
+
+// Published reviews
+
+exports.markReviewAsPublished = function(config, review) {
+  if (!review || !review.id || this.reviewPublished(review)) return;
 
   if (published_reviews.count >= REVIEWS_LIMIT) {
-    published_reviews.pop(published_reviews.count - (REVIEWS_LIMIT + 1))
+    published_reviews.pop(published_reviews.count - (REVIEWS_LIMIT + 1));
   }
-  published_reviews.unshift(review_id)
+  published_reviews.unshift(review.id);
 }
 
-var reviewPublished = function(config, review) {
-  var review_id = reviewId(config, review);
-  if (!review_id) return false;
-  return published_reviews.indexOf(review_id) > -1
+exports.reviewPublished = function(review) {
+  if (!review || !review.id) return false;
+  return published_reviews.indexOf(review.id) >= 0;
 }
 
-var reviewId = function(config, review) {
-  if (config.store === REVIEWS_STORES.APP_STORE)  {
-    return review['id'];
-  } else if (config.store === REVIEWS_STORES.GOOGLE_PLAY) {
-    return review['guid'];
-  } else {
-    return null;
-  }
+exports.publishedReviews = function() {
+  return published_reviews;
 }
+
+exports.resetPublishedReviews = function() {
+  return published_reviews = [];
+}
+
+// App Store app information
 
 var isAppInformationEntry = function(entry) {
     // App information is available in an entry with some special fields
@@ -160,18 +248,29 @@ var updateAppInformation = function(config, entry, appInformation) {
   }
 }
 
-var slackMessage = function(review, config, appInformation) {
-  if (config.debug) console.log("INFO: Creating message for review " + review.title);
+exports.parseAppStoreReview = function(rssItem, config, appInformation) {
+  review = {};
 
-  var title = reviewTitle(review, config);
-  var rating = reviewRating(review, config);
-  var date = reviewDate(review, config);
-  var author = reviewAuthor(review, config);
-  var appLink = config.appLink ? config.appLink : appInformation.appLink;
+  review.id = rssItem['id'];
+  review.title = rssItem.title;
+  review.text = rssItem.description;
+  review.rating = reviewRating(rssItem, config);
+  review.date = reviewDate(rssItem, config);
+  review.author = rssItem.author;
+  review.link = config.appLink ? config.appLink : appInformation.appLink;
+  review.storeName = "App Store";
+
+  return review;
+}
+
+// Slack
+
+exports.slackMessage = function(review, config, appInformation) {
+  if (config.debug) console.log("INFO: Creating message for review " + review.title);
 
   var stars = "";
   for (var i = 0; i < 5; i++) {
-    stars += i < rating ? "★" : "☆";
+    stars += i < review.rating ? "★" : "☆";
   }
 
   var pretext = "New review";
@@ -180,18 +279,18 @@ var slackMessage = function(review, config, appInformation) {
   }
   pretext += "!";
 
-  var color = rating >= 4 ? "good" : (rating >= 2 ? "warning" : "danger");
+  var color = review.rating >= 4 ? "good" : (review.rating >= 2 ? "warning" : "danger");
 
   var text = "";
-  text += review.description + "\n";
-  text += "_by " + author;
-  if (date) {
-    text += ", " + date;
+  text += review.text + "\n";
+  text += "_by " + review.author;
+  if (review.date) {
+    text += ", " + review.date;
   }
-  if (appLink) {
-    text += " - " + "<" + appLink + "|" + storeName(config) + ">";
+  if (review.link) {
+    text += " - " + "<" + review.link + "|" + review.storeName + ">";
   } else {
-    text += " - " + storeName(config);
+    text += " - " + review.storeName;
   }
   text += "_";
 
@@ -202,7 +301,7 @@ var slackMessage = function(review, config, appInformation) {
     "attachments": [
       {
         "mrkdwn_in": ["text", "pretext", "title"],
-        "fallback": pretext + ": " + title + " (" + stars + "): " + review.description,
+        "fallback": pretext + ": " + review.title + " (" + stars + "): " + review.text,
 
         "pretext": pretext,
         "color": color,
@@ -210,8 +309,8 @@ var slackMessage = function(review, config, appInformation) {
         "author_name": stars,
         "author_icon": config.appIcon ? config.appIcon : appInformation.appIcon,
 
-        "title": title,
-        "title_link": appLink,
+        "title": review.title,
+        "title_link": review.link,
 
         "text": text
       }
@@ -221,13 +320,22 @@ var slackMessage = function(review, config, appInformation) {
   return message;
 }
 
-var reviewTitle = function(review, config) {
-  if (config.store === REVIEWS_STORES.APP_STORE)  {
-    return review.title;
-  } else if (config.store === REVIEWS_STORES.GOOGLE_PLAY) {
-    return null; // Google Play reviews does not have title
-  } else {
-    return null;
+var welcomeMessage = function(config, appInformation) {
+  var storeName = config.store === REVIEWS_STORES.APP_STORE ? "App Store" : "Google Play"; 
+  var appName = config.appName ? config.appName : (appInformation.appName ? appInformation.appName : config.appId);
+  return {
+    "username": config.botUsername,
+    "icon_url": config.botIcon,
+    "channel": config.channel,
+    "attachments": [
+      {
+        "mrkdwn_in": ["pretext", "author_name"],
+        "fallback": "This channel will now receive " + storeName + " reviews for " + appName,
+        "pretext": "This channel will now receive " + storeName + " reviews for ",
+        "author_name": appName,
+        "author_icon": config.appIcon ? config.appIcon : appInformation.appIcon
+      }
+    ]
   }
 }
 
@@ -246,23 +354,13 @@ var reviewDate = function(review, config) {
   if (config.store === REVIEWS_STORES.APP_STORE)  {
     return review.date.toISOString().replace("T", " ").substr(0, 16);
   } else if (config.store === REVIEWS_STORES.GOOGLE_PLAY) {
-    return null; // Google Play review RSS dates are often unreliable (current date instead of review date)
+    return undefined; // Google Play review RSS dates are often unreliable (current date instead of review date)
   } else {
-    return null;
+    return undefined;
   }
 }
 
-var reviewAuthor = function(review, config) {
-  if (config.store === REVIEWS_STORES.APP_STORE)  {
-    return review.author;
-  } else if (config.store === REVIEWS_STORES.GOOGLE_PLAY) {
-    return review.title.substr(4);
-  } else {
-    return null;
-  }
-}
-
-var postToSlack = function(message, config) {
+exports.postToSlack = function(message, config) {
   messageJSON = JSON.stringify(message);
   if (config.debug) console.log("INFO: Posting new message to Slack: " + messageJSON);
   return result = request.post({
---
+++ index.spec.js
@@ -0,0 +1,220 @@
+const sinon = require('sinon')
+const chai = require('chai')
+const expect = require('chai').expect
+const sinonChai = require('sinon-chai')
+
+const Watcher = require('rss-watcher');
+const request = require('request');
+
+const reviews = require('./index')
+
+before(function () {
+  chai.use(sinonChai)
+})
+
+beforeEach(function () {
+  this.sandbox = sinon.sandbox.create()
+  this.clock = sinon.useFakeTimers()
+})
+
+afterEach(function () {
+  this.sandbox.restore()
+  this.clock.restore()
+
+  reviews.resetPublishedReviews()
+})
+
+describe('The module', function () {
+  it('marks review as published', function * () {
+    const review = {id: '123'}
+    const config = {}
+
+    markReviewAsPublishedSpy = this.sandbox.spy(reviews, 'markReviewAsPublished')
+    reviewPublishedSpy = this.sandbox.spy(reviews, 'reviewPublished')
+
+    expect(reviews.publishedReviews().length).to.eql(0)
+
+    reviews.markReviewAsPublished(config, review)
+
+    expect(markReviewAsPublishedSpy.callCount).to.eql(1)
+    expect(reviewPublishedSpy.callCount).to.eql(1)
+
+    expect(reviews.publishedReviews().length).to.eql(1)
+    expect(reviews.reviewPublished(review)).to.be.true
+  })
+
+  it('does not mark other review as published', function * () {
+    const review = {id: '123'}
+    const otherReview = {id: '456'}
+    const config = {}
+
+    expect(reviews.publishedReviews().length).to.eql(0)
+
+    reviews.markReviewAsPublished(config, review)
+
+    expect(reviews.reviewPublished(review)).to.be.true
+    expect(reviews.reviewPublished(otherReview)).to.be.false
+  })
+
+  it('resolves store to App Store', function * () {
+    const config = {appId: '123'}
+
+    const watcherSetStub = this.sandbox.stub(Watcher.prototype, 'set', function (settings) {
+      return;
+    })
+    expect(watcherSetStub.callCount).to.eql(0)
+    reviews.start(config)
+    expect(watcherSetStub.callCount).to.eql(1)
+    expect(config.store).to.eql('app-store')
+  })
+
+  it('resolves store to Google Play', function * () {
+    const config = {appId: 'com.google.play'}
+    const fetchGooglePlayReviewsStub = this.sandbox.stub(reviews, 'fetchGooglePlayReviews', function (config, appInformation) {
+      return [];
+    })
+    expect(fetchGooglePlayReviewsStub.callCount).to.eql(0)
+    reviews.start(config)
+    expect(fetchGooglePlayReviewsStub.callCount).to.eql(1)
+    expect(config.store).to.eql('google-play')
+  })
+
+  it('parses App Store RSS item', function * () {
+  	const config = {
+  		appId: '123',
+  		appLink: 'http://www.google.com',
+  		store: 'app-store'
+  	}
+  	const theDate = new Date('2016-09-14T05:58:00-07:00')
+    const rssItem = {
+    	id: '123',
+    	title: 'the title of the review',
+      description: 'the text of the review',
+    	author: 'the author',
+    	date: theDate,
+    	'im:rating': {
+    		'#': 3
+    	}
+    }
+
+    review = reviews.parseAppStoreReview(rssItem, config, {})
+
+    expect(review.id).to.eql('123')
+    expect(review.title).to.eql('the title of the review')
+    expect(review.text).to.eql('the text of the review')
+    expect(review.rating).to.eql(3)
+    expect(review.date).to.eql('2016-09-14 12:58')
+    expect(review.author).to.eql('the author')
+    expect(review.link).to.eql('http://www.google.com')
+    expect(review.storeName).to.eql('App Store')
+  })
+
+  it('generates Slack message from review', function * () {
+    const config = {
+      appName: 'the app name',
+      appIcon: 'https://i.imgur.com/BoT.jpg',
+      botUsername: 'the bot\'s username',
+      botIcon: 'http://i.imgur.com/asdF.jpg',
+      channel: '#notestforthewicked',
+      store: 'app-store'
+    }
+    const review = {
+    	title: 'the title',
+      text: 'the text',
+    	rating: 2,
+    	link: 'http://www.google.com',
+      author: 'the author',
+      storeName: 'App Store'
+    }
+
+    message = reviews.slackMessage(review, config, {})
+
+    expect(message.username).to.eql('the bot\'s username')
+    expect(message.icon_url).to.eql('http://i.imgur.com/asdF.jpg')
+    expect(message.channel).to.eql('#notestforthewicked')
+
+    const attachment = message.attachments[0]
+
+    expect(attachment.mrkdwn_in).to.eql(["text", "pretext", "title"])
+    expect(attachment.fallback).to.eql('New review for ' + config.appName + '!: ' + review.title + ' (★★☆☆☆): ' + review.text)
+
+    expect(attachment.pretext).to.eql('New review for ' + config.appName + '!')
+    expect(attachment.color).to.eql('warning')
+
+    expect(attachment.author_name).to.eql('★★☆☆☆')
+    expect(attachment.author_icon).to.eql('https://i.imgur.com/BoT.jpg')
+
+    expect(attachment.title).to.eql('the title')
+    expect(attachment.title_link).to.eql('http://www.google.com')
+
+    expect(attachment.text).to.eql(review.text + '\n_by the author - <http://www.google.com|App Store>_')
+  })
+
+  it('recognizes new Google Play reviews', function * () {
+    const config = {
+      appId: 'com.mock.id',
+      store: 'google-play',
+      interval: 1
+    }
+
+    const initialReviews = [
+      {id: 123},
+      {id: 456}
+    ]
+
+    const newReviews = [
+      {id: 123},
+      {id: 456},
+      {id: 789}
+    ]
+
+    const reviewsFetchGooglePlayReviewsStub = this.sandbox.stub(reviews, 'fetchGooglePlayReviews', function (config, appInformation, callback) {
+      switch (reviewsFetchGooglePlayReviewsStub.callCount) {
+        case 1:
+          // First call
+          callback(initialReviews)
+          break
+        case 2:
+          // Second call
+          callback(initialReviews)
+          break
+        case 3:
+          // Third call
+          callback(newReviews)
+          break
+        default:
+          break
+      }
+      return
+    })
+
+    const reviewsPostToSlackStub = this.sandbox.stub(reviews, 'postToSlack')
+
+    expect(reviewsFetchGooglePlayReviewsStub.callCount).to.eql(0)
+    expect(reviewsPostToSlackStub.callCount).to.eql(0)
+
+    // First call
+    reviews.start(config)
+
+    expect(reviewsFetchGooglePlayReviewsStub.callCount).to.eql(1)
+    expect(reviewsPostToSlackStub.callCount).to.eql(0)
+
+    this.clock.tick(1000);
+
+    // Second call
+    expect(reviewsFetchGooglePlayReviewsStub.callCount).to.eql(2)
+    expect(reviewsPostToSlackStub.callCount).to.eql(0)
+
+    this.clock.tick(1000);
+
+    // Third call (should post a new message to Slack)
+    expect(reviewsFetchGooglePlayReviewsStub.callCount).to.eql(3)
+    expect(reviewsPostToSlackStub.callCount).to.eql(1)
+
+    this.clock.tick(1000);
+
+    // Fourth call
+    expect(reviewsFetchGooglePlayReviewsStub.callCount).to.eql(4)
+    expect(reviewsPostToSlackStub.callCount).to.eql(1)
+  })
+})
